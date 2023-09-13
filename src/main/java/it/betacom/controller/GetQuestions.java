79f8909b91edbcbece0264153527d9f8846ae805
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-09-13 11:05:57 +0200

    getting the single question and adding the number of time a choice is selected

--- src/main/java/it/betacom/controller/GetQuestions.java
+++ src/main/java/it/betacom/controller/GetQuestions.java
@@ -46,30 +46,44 @@ public class GetQuestions extends HttpServlet {
 		JSONArray jsonArray = new JSONArray();
 		try {
 			Connection con = Database.getConnection();
-			String sql = "select * from questions";
+			String questionParam = request.getParameter("questionId");
 
-			PreparedStatement ps = con.prepareStatement(sql);
+			if (questionParam != null) {
+				int questionId = Integer.parseInt(questionParam);
+				String sql = "select * from questions where questionId = ?";
 
-			ResultSet rs = ps.executeQuery();
-			ResultSetMetaData rsmd = rs.getMetaData();
-			int columnCount = rsmd.getColumnCount();
+				PreparedStatement ps = con.prepareStatement(sql);
+				ps.setInt(1, questionId);
+				ResultSet rs = ps.executeQuery();
+				ResultSetMetaData rsmd = rs.getMetaData();
+				int columnCount = rsmd.getColumnCount();
 
-			while (rs.next()) {
-				JSONObject jsonObject = new JSONObject();
-				for (int i = 1; i <= columnCount; i++) {
-					String columnName = rsmd.getColumnName(i);
-					Object value = rs.getObject(columnName);
-					jsonObject.put(columnName, value);
+				while (rs.next()) {
+					JSONObject jsonObject = new JSONObject();
+					for (int i = 1; i <= columnCount; i++) {
+						String columnName = rsmd.getColumnName(i);
+						Object value = rs.getObject(columnName);
+						jsonObject.put(columnName, value);
+					}
+					jsonArray.put(jsonObject);
 				}
-				jsonArray.put(jsonObject);
+				System.out.println(jsonArray.toString());
+
+				response.setContentType("application/json");
+				response.getWriter().write(jsonArray.toString());
+			} else {
+				System.out.println("You haven't provided the questionId param");
 			}
-			System.out.println(jsonArray.toString());
-
-			response.setContentType("application/json");
-			response.getWriter().write(jsonArray.toString());
-
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} catch (NumberFormatException e) {
+
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 400 Bad Request
+			response.getWriter().write("Invalid 'questionId' parameter");
+
+			// Handle the case where the "questionId" parameter cannot be parsed as an
+			// integer
+			// You may want to send an error response or handle it in another way.
 		}
 
 	}
@@ -85,20 +99,65 @@ public class GetQuestions extends HttpServlet {
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
 		try {
 			Connection con = Database.getConnection();
-			 String sql = "insert into questions (question_text, time_selected) values (?, 0)";
-	            
-	            try (PreparedStatement ps = con.prepareStatement(sql)) {
-	                ps.setString(1, request.getParameter("question"));
-	                
-	                int rowsAffected = ps.executeUpdate(); 
-	                if(rowsAffected <= 0) {
-	                    // Handle the case where no rows were affected
-	                }
-	            }
-			
+			String sql = "insert into questions (question_text, time_selected) values (?, 0)";
+
+			try (PreparedStatement ps = con.prepareStatement(sql)) {
+				ps.setString(1, request.getParameter("question"));
+
+				int rowsAffected = ps.executeUpdate();
+				if (rowsAffected <= 0) {
+					// Handle the case where no rows were affected
+				}
+			}
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    response.setHeader("Access-Control-Allow-Origin", "*");
+	    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	    response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
+	    
+	    try {
+	        Connection con = Database.getConnection();
+	        String questionIdParam = request.getParameter("questionId");
+	        String questionTextParam = request.getParameter("questionText");
+
+	        if (questionIdParam != null && questionTextParam != null) {
+	            int questionId = Integer.parseInt(questionIdParam);
+	            int questionText = Integer.parseInt(questionTextParam);
+	            String sql = "";
+	            if (questionText == 1) {
+	                sql = "UPDATE questions SET time_selected_first_question = time_selected_first_question + 1 WHERE questionId = ?";
+	            } else if (questionText == 2) {
+	                sql = "UPDATE questions SET time_selected_second_question = time_selected_second_question + 1 WHERE questionId = ?";
+	            } else {
+	                response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 400 Bad Request
+	                response.getWriter().write("The 'questionText' param must be 1 or 2");
+	                return;
+	            }
+
+	            PreparedStatement ps = con.prepareStatement(sql);
+	            ps.setInt(1, questionId);
+	            int updatedRows = ps.executeUpdate();
+
+	            response.setContentType("application/json");
+	            response.getWriter().write("{\"updatedRows\": " + updatedRows + "}");
+	        } else {
+	            response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 400 Bad Request
+	            response.getWriter().write("You haven't provided the required parameters");
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); // 500 Internal Server Error
+	        response.getWriter().write("Database error");
+	    } catch (NumberFormatException e) {
+	        response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 400 Bad Request
+	        response.getWriter().write("Invalid parameter values");
+	    }
+	}
+
 }
